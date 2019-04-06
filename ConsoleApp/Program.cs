083e05919ddf6623159f6285d233adbe6b7a003f
Takayuki Kamiyama <karuma.reason@gmail.com>
2019-04-06 12:03:03 +0900

    default source tree.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("ConsoleApp Start");
+        }
+    }
+}
