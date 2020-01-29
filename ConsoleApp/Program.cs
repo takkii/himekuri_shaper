04104eb0976a9037a537178bdb6fea8013bf47bc
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-01-29 17:25:21 +0900

    Add, Exception catch.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,28 +6,42 @@ namespace ConsoleApp
     {
         static void Main()
         {
-            // Method
-            DateTime dt = DateTime.Now;
-            string str = "令和";
-            string str2 = "R0";
-            string ver = "1.0.0";
+            try
+            {
+                // 宣言
+                DateTime dt = DateTime.Now;
+                string str = "令和";
+                string str2 = "R0";
 
-            // Calc
-            var nengo = (dt.Year + "年" + dt.Month + "月" + dt.Day + "日" + " : " + dt.Hour + "時" + dt.Minute + "分" + dt.Second + "秒");
-            var reiwa = (str + (dt.Year - 2018) + "年"  + dt.Month + "月" + dt.Day + "日");
-            var reiwa2 = (str2 + (dt.Year - 2018) + "." + dt.Month + "." + dt.Day);
+                // 計算
+                var nengo = (dt.Year + "年" + dt.Month + "月" + dt.Day + "日" + " : " + dt.Hour + "時" + dt.Minute + "分" +
+                             dt.Second + "秒");
+                var reiwa = (str + (dt.Year - 2018) + "年" + dt.Month + "月" + dt.Day + "日");
+                var reiwa2 = (str2 + (dt.Year - 2018) + "." + dt.Month + "." + dt.Day);
 
-            // Count 
-            var count = new DateTime(dt.Year , dt.Month, dt.Day);
-            var count2 = new DateTime(dt.Year + 1,1,1);
-            double oshogatsu = (count2 - count).TotalDays;
+                // 変換 
+                var count = new DateTime(dt.Year, dt.Month, dt.Day);
+                var count2 = new DateTime(dt.Year + 1, 1, 1);
+                double oshogatsu = (count2 - count).TotalDays;
 
-            // Console
-            Console.WriteLine(nengo);
-            Console.WriteLine(reiwa + " : " +  reiwa2);
-            Console.WriteLine("来年の1月1日まであと：" +(oshogatsu - 1) + "日です。");
-            Console.WriteLine("日めくりの数え番号：" + ver);
-            Console.ReadKey();
+                // 出力
+                Console.WriteLine(nengo);
+                Console.WriteLine(reiwa + " : " + reiwa2);
+                Console.WriteLine("来年の1月1日まであと：" + (oshogatsu - 1) + "日です。");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Log : " + e);
+            }
+            finally
+            {
+                // バージョン
+                string ver = "1.0.0";
+
+                // 出力
+                Console.WriteLine("日めくりの数え番号：" + ver);
+                Console.ReadKey();
+            }
         }
     }
 }
