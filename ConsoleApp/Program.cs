56a913a215a71ee78073c5c89ad9ee6e7193870f
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-02-02 14:18:23 +0900

    Update, sort in code.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -15,16 +15,17 @@ namespace ConsoleApp
                 const string str2 = "R0";
 
                 // 計算
-                var nengo = (dt.Year + "年" + dt.Month + "月" + dt.Day + "日" + " : " + dt.Hour + "時" + dt.Minute + "分" +
-                             dt.Second + "秒");
                 var reiwa = (str + (dt.Year - 2018) + "年" + dt.Month + "月" + dt.Day + "日");
                 var reiwa2 = (str2 + (dt.Year - 2018) + "." + dt.Month + "." + dt.Day);
 
-                // 変換 
+                // 入れ子
                 var count = new DateTime(dt.Year, dt.Month, dt.Day);
                 var count2 = new DateTime(dt.Year + 1, 1, 1);
                 var gantan = (count2 - count).TotalDays;
 
+                // ソート対象
+                var nengo = (dt.Year + "年" + dt.Month + "月" + dt.Day + "日" + " : " + dt.Hour + "時" + dt.Minute + "分" +
+                             dt.Second + "秒");
                 var hagoita = reiwa + " : " + reiwa2;
                 var oshogatsu = "来年の1月1日まであと：" + (gantan - 1) + "日です。";
 
