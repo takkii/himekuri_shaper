d48776f988a9709712f986601dbd919be11f370d
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-03-22 19:21:47 +0900

    Delete, no need word.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp
 {
     public static class Himekuri
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
             try
             {
