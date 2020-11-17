e36f2bbb56d50e30e5da822ddf31eeebc1b026d0
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-11-17 19:01:12 +0900

    Update, const string.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -12,12 +12,22 @@ namespace ConsoleApp
             {
                 // 宣言
                 var dt = DateTime.Now;           
-                const string str = "令和";
-                const string str2 = "R0";
+                const string reiwa_kanji = "令和";
+                const string reiwa_alpha = "R0";
+                const string OneYear = "年";
+                const string OneMonth = "月";
+                const string Onedays = "日";
+                const string OneHour = "時";
+                const string OneMinutes = "分";
+                const string Oneseconds = "秒";
+                const string comma = ".";
+                const string koron = " : ";
+                const string next_year = "来年の1月1日まであと：";
+                const string message = "日です";
 
                 // 計算
-                var reiwa = (str + (dt.Year - 2018) + "年" + dt.Month + "月" + dt.Day + "日");
-                var reiwa2 = (str2 + (dt.Year - 2018) + "." + dt.Month + "." + dt.Day);
+                var reiwa = (reiwa_kanji + (dt.Year - 2018) + OneYear + dt.Month + OneMonth + dt.Day + Onedays);
+                var reiwa2 = (reiwa_alpha + (dt.Year - 2018) + comma + dt.Month + comma + dt.Day);
 
                 // 入れ子
                 var count = new DateTime(dt.Year, dt.Month, dt.Day);
@@ -25,10 +35,10 @@ namespace ConsoleApp
                 var gantan = (count2 - count).TotalDays;
 
                 // 参照先
-                var nengo = (dt.Year + "年" + dt.Month + "月" + dt.Day + "日" + " : " + dt.Hour + "時" + dt.Minute + "分" +
-                             dt.Second + "秒" + " : " + dt.ToString("dddd"));
+                var nengo = (dt.Year + OneYear + dt.Month + OneMonth + dt.Day + Onedays + koron + dt.Hour + OneHour + dt.Minute + OneMinutes +
+                             dt.Second + OneMinutes + " : " + dt.ToString("dddd"));
                 var hagoita = reiwa + " : " + reiwa2;
-                var oshogatsu = "来年の1月1日まであと：" + (gantan - 1) + "日です。";
+                var oshogatsu = next_year + (gantan - 1) + message ;
 
                 // 安定ソート
                 var takoage = new[]
@@ -55,7 +65,7 @@ namespace ConsoleApp
                 try
                 {
                     // 宣言
-                    const string ver = "1.0.1";
+                    const string ver = "1.0.2";
                     const string himekuriShaperVersion = "日めくりの数え番号：" + ver;
 
                     // 安定ソート
